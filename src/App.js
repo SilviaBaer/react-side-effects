1fb42955b175a638339509e4b213638551250453
SilviaBarcellini <77615477+SilviaBarcellini@users.noreply.github.com>
2021-06-15 17:56:35 +0200

    ✨ store info with useEffect and localStorage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -7,10 +7,25 @@ import MainHeader from './components/MainHeader/MainHeader';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  //store const storedUserLoggedInInfo into useEffect in order to prevent infinite loop
+  //dependencies never change, the dep just change the first time the app runs
+  //if I reload I stay logged in, because the info are now stored into localStorage
+  //it runs after useState
+  //but not everytime the components get reevaluate 
+  //only when the dependencies change (in this specific scenario NEVER except the very first time the app runs)
+
+  useEffect(() => {
+    const storedUserLoggedInInfo = localStorage.getItem("isLoggedIn"); 
+
+    if (storedUserLoggedInInfo === "1") {
+      setIsLoggedIn(true);
+    }
+  }, [])
+  
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
-    // eslint-disable-next-line
+
     localStorage.setItem("isLoggedIn", "1")
     setIsLoggedIn(true);
   };
